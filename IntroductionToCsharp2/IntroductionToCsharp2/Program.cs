3fabadc66c5069d415ebba1c4cc9ad8fe128e66b
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-02-06 16:26:39 +0100

    update

--- IntroductionToCsharp2/IntroductionToCsharp2/Program.cs
+++ IntroductionToCsharp2/IntroductionToCsharp2/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Please enter your name");
-
 string UserName = Console.ReadLine();
 
-Console.WriteLine("Hello {0}",UserName);
+Console.WriteLine("Please enter your last name");
+string LastName = Console.ReadLine();
+
+Console.WriteLine("Hello {0} {1}",UserName,LastName);
 //Console.WriteLine("Hello " + Username);
