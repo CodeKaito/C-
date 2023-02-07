d9dda113f87add01cf027461f786d66e840c375e
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-02-07 13:25:42 +0100

    update

---
+++ DoWhileLoop/DoWhileLoop/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please enter your target");
+int UserTarget = int.Parse(Console.ReadLine());
+
+int Start = 0;
+
+while (Start<=UserTarget)
+{
+    Console.Write(Start + "");
+    Start++;
+}
+
+string UserChoice = "";
+
+do
+{
+    Console.WriteLine("Do you want to continue - Yes or No ?");
+
+    if (UserChoice != "Yes" && UserChoice != "No")
+    {
+        Console.WriteLine("Invalid choice");
+    } while (UserChoice != "Yes" && UserChoice != "No")
+    {
+
+    }
+}
