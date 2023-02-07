68189f01d5dea971f4e76cc8536b7085959b6d3a
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-02-07 12:59:14 +0100

    add WhileLoop

---
+++ WhileLoop/WhileLoop/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please enter your target?");
+int UserTarget = int.Parse(Console.ReadLine());
+
+int Start = 0;
+
+while (Start <= UserTarget)
+{
+    Console.WriteLine(Start);
+    Start++;
+}
