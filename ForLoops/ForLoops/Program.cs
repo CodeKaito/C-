f5996435589f19a2347653bb24c6d6ae9a7a2ce1
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-02-07 14:42:29 +0100

    update foor loop

--- ForLoops/ForLoops/Program.cs
+++ ForLoops/ForLoops/Program.cs
@@ -4,6 +4,11 @@ Numbers[0] = 101;
 Numbers[1] = 102;
 Numbers[2] = 103;
 
+for (int j = 0; j <Numbers.Length; j++)
+{
+    Console.WriteLine(Numbers[j]);
+}
+
 int i = 0;
 while (1 < Numbers.Length)
 {
