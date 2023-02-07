8df41956cbf41f87d20277c3049c822db579b3d9
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-02-07 12:01:18 +0100

    add DataTypes

---
+++ DataTypes/DataTypes/Program.cs
@@ -0,0 +1,7 @@
+﻿int myNum = 5;               // Integer (whole number)
+double myDoubleNum = 5.99D;  // Floating point number
+char myLetter = 'D';         // Character
+bool myBool = true;          // Boolean
+string myText = "Hello";     // String
+
+Console.WriteLine(myNum);
