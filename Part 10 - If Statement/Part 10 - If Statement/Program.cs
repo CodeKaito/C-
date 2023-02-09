9056d1f875b2093f7bc101c115867696ad9cc50d
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-02-09 16:01:43 +0100

    if statement

--- Part 10 - If Statement/Part 10 - If Statement/Program.cs
+++ Part 10 - If Statement/Part 10 - If Statement/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Please enter a number");
+
+int UserNumber = int.Parse(Console.ReadLine());
+
+if (UserNumber == 1)
+{
+    Console.WriteLine("your number is 1");
+}
+if (UserNumber == 2)
+{
+    Console.WriteLine("your number is 2");
+}
+if (UserNumber == 3)
+{
+    Console.WriteLine("your numbers is 3");
+}
+if (UserNumber != 1 && UserNumber != 2 && UserNumber != 3)
+{
+    Console.WriteLine("your number is not between 1 and 3");
+}
+
