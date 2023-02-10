805f2b9d21dca4cdd5e1a43a4050a5b089fa55f3
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-02-10 11:00:17 +0100

    add if statement

---
+++ Part 10 - If Statement (ShortHand)/Part 10 - If Statement (ShortHand)/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Insert a number");
+
+int time = int.Parse(Console.ReadLine());
+/*
+if (time < 18)
+{
+    Console.WriteLine("Good day.");
+}
+else
+{
+    Console.WriteLine("Good evening.");
+}
+*/
+
+// variable = (condition) ? expressionTrue :  expressionFalse;
+
+string result = (time < 18) ? "Good day.." : "Good evening..";
+Console.WriteLine(result);
--- Part 10 - If Statement (part2)/Part 10 - If Statement (part2)/Program.cs
+++ Part 10 - If Statement (part2)/Part 10 - If Statement (part2)/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine("Please enter a number");
 
 int UserNumber = int.Parse(Console.ReadLine());
-
+/*
 if (UserNumber == 10 | UserNumber == 20)
 {
     Console.WriteLine("Your number is 10 or 20");
@@ -11,7 +11,9 @@ else
 {
     Console.WriteLine("Your number is not 10 either 20");
 }
-
+*/
 // && - & means and 
 // || - | means or
 
+string result = (UserNumber == 10 | UserNumber == 20) ? "Your number is 10 or 20.." : "Your number is not 10 either 20..";
+Console.WriteLine(result);
