1b31feed9928c4e12f8bc5a36b65bec847fb4ddc
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-02-10 10:00:40 +0100

    add example

---
+++ Part 10 - If Statement (part2)/Part 10 - If Statement (part2)/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please enter a number");
+
+int UserNumber = int.Parse(Console.ReadLine());
+
+if (UserNumber == 10 | UserNumber == 20)
+{
+    Console.WriteLine("Your number is 10 or 20");
+}
+else
+{
+    Console.WriteLine("Your number is not 10 either 20");
+}
--- Part 10 - If Statement/Part 10 - If Statement/Program.cs
+++ Part 10 - If Statement/Part 10 - If Statement/Program.cs
@@ -7,15 +7,15 @@ if (UserNumber == 1)
 {
     Console.WriteLine("your number is 1");
 }
-if (UserNumber == 2)
+else if (UserNumber == 2)
 {
     Console.WriteLine("your number is 2");
 }
-if (UserNumber == 3)
+else if (UserNumber == 3)
 {
     Console.WriteLine("your numbers is 3");
 }
-if (UserNumber != 1 && UserNumber != 2 && UserNumber != 3)
+else (UserNumber != 1 && UserNumber != 2 && UserNumber != 3)
 {
     Console.WriteLine("your number is not between 1 and 3");
 }
