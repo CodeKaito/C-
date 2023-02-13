1f1cf6aa288b2037cf21ec656419d5723935fe9c
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-02-13 15:08:05 +0100

    add method parameters

---
+++ Part 16 - Method Parameters/Part 16 - Method Parameters/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        int i = 0;
+
+        SimpleMethod(ref i);
+
+        // i = 0;
+
+        Console.WriteLine(i);
+    }
+
+    public static void SimpleMethod(ref int j)
+    {
+        j = 100;
+    }
+}
