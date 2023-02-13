6a772ae52253dfa0b31b61d87f6f6a6c5aa5ac20
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-02-13 15:53:10 +0100

    update

--- Part 16 - Method Parameters/Part 16 - Method Parameters/Program.cs
+++ Part 16 - Method Parameters/Part 16 - Method Parameters/Program.cs
@@ -1,10 +1,12 @@
 ﻿class Program
 {
-    public static void Main()
+    public static void Main(string[] args)
     {
         int i = 0;
 
         SimpleMethod(ref i);
+
+        Saluta();
 
         // i = 0;
 
@@ -14,5 +16,10 @@
     public static void SimpleMethod(ref int j)
     {
         j = 100;
+    }
+
+    static void Saluta()
+    {
+        Console.WriteLine("Ciao");
     }
 }
