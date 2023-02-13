1fb2ecc19e5e722e8e986a307cac9a3ca39a0533
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-02-13 14:33:56 +0100

    add part 2

---
+++ Part 16 - Method (part 2)/Part 16 - Method (part 2)/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+
+    public static void Main()
+    {
+        EvenNumbers(30);
+
+        Program P = new Program();
+        int Sum = P.Add(10, 20);
+
+        Console.WriteLine("Sum = {0}", Sum);
+    }
+
+    public int Add (int FN, int SN)
+    {
+        return FN + SN;
+    }
+
+    public static void EvenNumbers(int Target)
+    {
+        int Start = 0;
+
+        while (Start <= Target)
+        {
+            Console.WriteLine(Start);
+            Start++;
+        }
+    }
+}
