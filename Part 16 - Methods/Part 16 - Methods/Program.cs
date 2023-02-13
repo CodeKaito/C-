96da3c6091322437dc51cf71962840b76198fce1
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-02-13 11:15:55 +0100

    add part 16

---
+++ Part 16 - Methods/Part 16 - Methods/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Program p = new Program();
+        p.EvenNumbers();
+    }
+    public void EvenNumbers()
+    {
+        int Start = 0;
+
+        while (Start < 20)
+        {
+            Console.WriteLine(Start);
+
+            Start = Start + 1;
+        }
+    }
+}
